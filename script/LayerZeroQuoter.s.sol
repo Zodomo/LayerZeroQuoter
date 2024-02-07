2824c35486d308042f789ae9f7ba867b20045e02
Zodomo <zodomo@proton.me>
2024-02-07 11:00:56 -0600

    init

--- script/LayerZeroQuoter.s.sol
+++ script/LayerZeroQuoter.s.sol
@@ -2,8 +2,9 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
+import {LayerZeroQuoter} from "../src/LayerZeroQuoter.sol";
 
-contract CounterScript is Script {
+contract LayerZeroQuoterScript is Script {
     function setUp() public {}
 
     function run() public {
---
+++ test/LayerZeroQuoter.t.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: CC0-1.0
+pragma solidity ^0.8.23;
+
+import {Test, console} from "forge-std/Test.sol";
+import {LayerZeroQuoter} from "../src/LayerZeroQuoter.sol";
+
+contract CounterTest is Test {
+    LayerZeroQuoter public quoter;
+
+    function setUp() public {}
+}
